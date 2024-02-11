4b6a5998e093a0a08cae70174dc6a427b98e5082
SatoruInoue2003 <satoru.inoue2003@gmail.com>
2024-02-11 19:56:21 +0900

    feat: DevicePreviewの追加とLaunch.jsonを設定

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,4 @@
+import 'package:device_preview/device_preview.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter/services.dart';
 import 'package:flutter_localizations/flutter_localizations.dart';
@@ -6,13 +7,19 @@ import 'package:flutter_riverpod/flutter_riverpod.dart';
 
 import 'presentation/page/search_page.dart';
 
+const _devicePreviewStr = String.fromEnvironment('devicePreview');
+const _devicePreviewIfNeeded = (_devicePreviewStr == 'on');
+
 void main() {
   WidgetsFlutterBinding.ensureInitialized();
   SystemChrome.setPreferredOrientations([
     DeviceOrientation.portraitUp,
     DeviceOrientation.portraitDown,
   ]).then((_) {
-    runApp(const ProviderScope(child: MyApp()));
+    runApp(DevicePreview(
+      enabled: _devicePreviewIfNeeded,
+      builder: (context) => const ProviderScope(child: MyApp()),
+    ));
   });
 }
 
